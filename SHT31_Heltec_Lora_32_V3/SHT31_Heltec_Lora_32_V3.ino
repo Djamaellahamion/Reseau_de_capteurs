6cdbdefb8049ca695077d3058829d340884dca5e
NazarioMORGADO <133801771+NazarioMORGADO@users.noreply.github.com>
2023-05-16 19:30:58 +0200

    Add files via upload

---
+++ SHT31_Heltec_Lora_32_V3/SHT31_Heltec_Lora_32_V3.ino
@@ -0,0 +1,92 @@
+/*************************************************** 
+  This is an example for the SHT31-D Humidity & Temp Sensor
+
+  Designed specifically to work with the SHT31-D sensor from Adafruit
+  ----> https://www.adafruit.com/products/2857
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+ ****************************************************/
+ 
+#include <Arduino.h>
+#include <Wire.h>
+#include "HT_SSD1306Wire.h"
+#include "Adafruit_SHT31.h"
+
+#define SHT31_SDA 41
+#define SHT31_SCL 42
+
+bool wireStatus = Wire1.begin(SHT31_SDA, SHT31_SCL);
+
+
+bool enableHeater = false;
+uint8_t loopCnt = 0;
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31( &Wire1);
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("SHT31 test");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+
+  Serial.print("Heater Enabled State: ");
+  if (sht31.isHeaterEnabled())
+    Serial.println("ENABLED");
+  else
+    Serial.println("DISABLED");
+
+  factory_display.init();
+	factory_display.clear();
+	factory_display.display();
+}
+
+
+void loop() {
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+
+  
+  factory_display.drawString(0, 10, "Temp= ");
+  factory_display.drawString(50, 10, String(t));
+  factory_display.drawString(0, 30, "Humid= ");
+  factory_display.drawString(50, 30, String(h));
+  factory_display.display();
+  delay(100);
+  factory_display.clear();
+
+  delay(1000);
+  // Toggle heater enabled state every 30 seconds
+  // An ~3.0 degC temperature increase can be noted when heater is enabled
+  if (loopCnt >= 30) {
+    enableHeater = !enableHeater;
+    sht31.heater(enableHeater);
+    Serial.print("Heater Enabled State: ");
+    if (sht31.isHeaterEnabled())
+      Serial.println("ENABLED");
+    else
+      Serial.println("DISABLED");
+
+    loopCnt = 0;
+  }
+  loopCnt++;
+}
